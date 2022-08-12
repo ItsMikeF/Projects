df8e26edf1b3ad102bc5469e0e089cf48d120bbe
ItsMikeF <67874093+ItsMikeF@users.noreply.github.com>
2022-08-11 21:36:08 -0400

    venv

--- Python/main2.py
+++ Python/main2.py
@@ -1,5 +1,9 @@
-print("hello world")
-
 import requests
 
+print("hello world")
+
 response = requests.get('http://www.google.com')
+
+print(response.status_code)
+print(response.headers['content-type'])
+print(response.headers['content-length'])
